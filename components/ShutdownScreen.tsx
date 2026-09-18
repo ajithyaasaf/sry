8adb221ace187101cefe336c6b836432d250ccaf
ajithyaasaf <ajithkannan.ag@gmail.com>
2026-09-18 23:48:25 +0530

    code change

--- components/ShutdownScreen.tsx
+++ components/ShutdownScreen.tsx
@@ -38,7 +38,7 @@ export function ShutdownScreen() {
           </h1>
 
           <p className="text-sm text-[#F5E9E2]/60 font-sans max-w-xs mx-auto leading-relaxed">
-            Avantaye ennanu kelu.....
+            Ask him.....
           </p>
         </div>
       </motion.div>
